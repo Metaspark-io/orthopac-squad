4b9c75a8560e7dc0d42ad1197df6fc855ae9f087
Rick Metzger <rickymetz@gmail.com>
2019-02-28 14:18:11 -0600

    Comment out icon file until later

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,11 +5,6 @@ module.exports = {
 		imageUrl: 'https://i.imgur.com/Vz81GEl.png',
 		description: 'A Project to bootstrap your next Gatsby + Bulma site.',
 		keywords: `Web developer, Web, Developer, CSS, HTML, JS, Javascript, Gatsby, Bulma Developer, CSS3, HTML5, Seo, Starter`,
-		twitter: 'https://twitter.com/amanhimself',
-		github: `https://github.com/amandeepmittal`,
-		medium: 'https://medium.com/@amanhimself',
-		gatsby: 'https://www.gatsbyjs.org/',
-		bulma: 'https://bulma.io/',
 		siteUrl: `https://www.example.com`
 	},
 	plugins: [
@@ -32,7 +27,7 @@ module.exports = {
 				background_color: '#2980b9',
 				theme_color: '#2980b9',
 				display: 'standalone',
-				icon: 'src/images/gatsby-icon.png',
+				// icon: 'src/images/gatsby-icon.png',
 				orientation: 'portrait'
 			}
 		},
