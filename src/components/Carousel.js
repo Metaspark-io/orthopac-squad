69a3897b6b73cc105b33814a0b7cb673fba04e57
Rick Metzger <rickymetz@gmail.com>
2019-02-28 14:59:03 -0600

    Wrap flixity to ensure it builds without throwing a 'window is undefined' error

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -10,18 +10,21 @@ const flickityOptions = {
 }
 
 export default class Carousel extends Component {
-  render () {
-    return (
-      <div class="container py-3 mb-5">
-        <Flickity options={flickityOptions}>
-          <img src="https://via.placeholder.com/800x400?text=Hero1" />
-          <img src="https://via.placeholder.com/800x400?text=Hero2" />
-          <img src="https://via.placeholder.com/800x400?text=Hero3" />
-          <img src="https://via.placeholder.com/800x400?text=Hero4" />
-          <img src="https://via.placeholder.com/800x400?text=Hero5" />
-          <img src="https://via.placeholder.com/800x400?text=Hero6" />
-        </Flickity>
-      </div>
-    )
+  render() {
+    if (typeof window !== 'undefined') {
+      return (
+        <div class="container py-3 mb-5">
+          <Flickity options={flickityOptions}>
+            <img src="https://via.placeholder.com/800x400?text=Hero1" />
+            <img src="https://via.placeholder.com/800x400?text=Hero2" />
+            <img src="https://via.placeholder.com/800x400?text=Hero3" />
+            <img src="https://via.placeholder.com/800x400?text=Hero4" />
+            <img src="https://via.placeholder.com/800x400?text=Hero5" />
+            <img src="https://via.placeholder.com/800x400?text=Hero6" />
+          </Flickity>
+        </div>
+      )
+    }
+    return null
   }
 }
