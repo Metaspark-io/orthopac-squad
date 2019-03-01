c9a26a6595c7f304db74a04b79d9a2fbb015d1b4
Rick Metzger <rickymetz@gmail.com>
2019-03-01 11:41:40 -0600

    Replace svg backgrounds with png to improve scroll performace. Change background colors

--- src/components/VideoHero.js
+++ src/components/VideoHero.js
@@ -4,7 +4,7 @@ export default class VideoHero extends Component {
   render () {
     return (
       <div class="bg-secondary d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
-        <h1 class="text-white">OrthoPAC Sqaud</h1>
+        <h1 class="text-white">OrthoPAC Squad</h1>
       </div>
     )
   }
