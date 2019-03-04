ab48a20d2ab596418acea590e745c9a7d21b88c8
Rick Metzger <rickymetz@gmail.com>
2019-03-04 16:29:06 -0500

    Hide nav flavor text on mobile, hide search on mobile

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react'
 import { Link } from 'gatsby'
+import MediaQuery from 'react-responsive'
 
 export default class Nav extends Component {
   render () {
+    const navBreak = '960px'
     return (
       <div className="container-fluid py-2 bg-white sticky-top border-bottom">
         <nav className="nav nav-pills nav-fill">
           <a className="nav-item nav-link" href="/#issues">
             <b>The Issues</b>
-            <span class="d-block"><small>Learn about the battles AAOS fights</small></span>
+            <MediaQuery minWidth={navBreak}>
+              <span class="d-block"><small>Learn about the battles AAOS fights</small></span>
+            </MediaQuery>
           </a>
           <a className="nav-item nav-link" href="/#heroes">
             <b>The Heroes</b>
-            <span class="d-block"><small>Meet the OrthoPAC Squad!</small></span>
+            <MediaQuery minWidth={navBreak}>
+              <span class="d-block"><small>Meet the OrthoPAC Squad!</small></span>
+            </MediaQuery>
           </a>
           <Link className="nav-item nav-link" activeClassName="active" to="/survey">
-            <b>Survey</b>
-            <span class="d-block"><small>Got a minute? Discover your Ortho hero identity</small></span>
+            <b>Hero Quiz</b>
+            <MediaQuery minWidth={navBreak}>
+              <span class="d-block"><small>Got a minute? Discover your Ortho hero identity</small></span>
+            </MediaQuery>
           </Link>
           <a className="nav-item nav-link" href="https://www.aaos.org/Advocacy/PAC/?ssopc=1" target="_blank" rel="noopener noreferrer">
             <b>Join the Squad</b>
-            <span class="d-block"><small>Donate to be a Hero for medicine</small></span>
+            <MediaQuery minWidth={navBreak}>
+              <span class="d-block"><small>Donate to be a Hero for medicine</small></span>
+            </MediaQuery>
           </a>
         </nav>
       </div>
--- src/components/Ribbon.js
+++ src/components/Ribbon.js
@@ -9,18 +9,17 @@ export default class Ribbon extends Component {
       <div className="ribbon">
         <div className="container py-4">
           <div className="row">
-            <div className="col-md-7 col-sm-8">
+            <div className="col-12 col-md-7">
               <a href="https://www.aaos.org/" className="d-flex align-items-center h-100" rel="noopener noreferrer">
                 <img
                   src={aaosLogo}
                   alt="AAOS: American Academy of Orthopedic Surgeons | American Association of Orthopedic Surgeons"
-                  style={{
-                    width: 550,
-                    height: 39 }}
+                  className="img-fluid"
+                  style={{ maxHeight: 39 }}
                 />
               </a>
             </div>
-            <div className="col-md-5 col-sm-4">
+            <div className="d-none d-md-inline-block col-md-5">
               <div className="d-flex flex-wrap justify-content-end">
                 <a
                   href="https://sso.aaos.org/login.aspx?vi=8&vt=77dc2d42411a9e98420dd4aaa5809106ee3572701588e5e6ee0c58f79c1d6ca84d304858ffc57b32d68b99ef404de6c6d5f0081e337c3594184003d8545161658382b3e80ad4a05a3907d076217b4ac42967966f94bda931557ebc73b9c211e4384993a31b7cdbd482b74314e270da05c29829fccad3fdd505f9d189bd304aad07cad1d45d7aab1734025ec7db4eb3ad5ac98358e895b25d09bc4b7ccdeca5f50993d7fe12cd216914078a761e6e8806ddd1cad026278ce5fc71b2d1eb5c4a11"
