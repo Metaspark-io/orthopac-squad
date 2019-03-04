8eaefed72cfd1a9702dfc5d573fd9eabd47371e2
Sean Perkins <sean@mobility-labs.com>
2019-03-04 11:29:45 -0500

    Fix hero carousel

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,2 @@
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /react-flickity-component/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
 }
