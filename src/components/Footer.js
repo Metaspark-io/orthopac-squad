765437dad45635693c6f7760571c300c731e97dd
Rick Metzger <rickymetz@gmail.com>
2019-03-04 08:54:16 -0500

    Add footer social and disclaimer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { FaFacebookF, FaTwitter, FaYoutube, FaLinkedinIn, FaInstagram } from 'react-icons/fa'
+
 import logo from '../images/aaos-logo-white.svg'
 
 export default class Footer extends Component {
@@ -19,7 +21,7 @@ export default class Footer extends Component {
               </div>
             </div>
             <div class="col-sm-3 col-6">
-              <ul class="list-unstyled">
+              <ul class="list-unstyled link-list">
                 <li><a href="https://www.aaos.org/about/about/" target="_self">About AAOS</a></li>
                 <li><a href="https://www.aaos.org/strategicplan/" target="_self">Mission &amp; Vision</a></li>
                 <li><a href="https://www.aaos.org/ContactUs/" target="_self">Contact Us</a></li>
@@ -27,7 +29,7 @@ export default class Footer extends Component {
               </ul>
             </div>
             <div class="col-sm-3 col-6">
-              <ul class="list-unstyled">
+              <ul class="list-unstyled link-list">
                 <li><a href="https://www.aaos.org/privacy.aspx" target="_self">PRIVACY POLICY</a></li>
                 <li><a href="https://www.aaos.org/uploadedFiles/PreProduction/About/Policies/meetings-anti-harassment-policy-and-procedures-aug-2018.pdf" target="_self">Anti-Harassment Policy</a></li>
                 <li><a href="https://www.aaos.org/About/Careers/" target="_self">Careers at AAOS</a></li>
@@ -36,8 +38,56 @@ export default class Footer extends Component {
               </ul>
             </div>
             <div class="col-sm-3">
+              <ul class="list-inline">
+                <li class="footer-social-icons__title">Members</li>
+                <li class="social-icon">
+                  <a href="https://www.facebook.com/AAOSMembers/" target="_self">
+                    <FaFacebookF/>
+                  </a>
+                </li>
+                <li class="social-icon">
+                  <a href="https://twitter.com/aaosmembers" target="_self">
+                    <FaTwitter/>
+                  </a>
+                </li>
+              </ul>
+              <ul class="list-inline">
+                <li class="footer-social-icons__title">public</li>
+                <li class="social-icon">
+                  <a href="http://www.facebook.com/aaos1" target="_self">
+                    <FaFacebookF/>
+                  </a>
+                </li>
+
+                <li class="social-icon">
+                  <a href="http://twitter.com/aaos1" target="_self">
+                    <FaTwitter/>
+                  </a>
+                </li>
+
+                <li class="social-icon">
+                  <a href="https://www.youtube.com/user/AmerAcadOrthoSurg" target="_self">
+                    <FaYoutube/>
+                  </a>
+                </li>
+
+                <li class="social-icon">
+                  <a href="https://www.linkedin.com/company/american-academy-of-orthopaedic-surgeons" target="_self">
+                    <FaLinkedinIn/>
+                  </a>
+                </li>
+
+                <li class="social-icon">
+                  <a href="https://www.instagram.com/aaos_1/" target="_self">
+                    <FaInstagram/>
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
+          <small class="text-center d-flex pt-5 pb-3">
+            © 1995-2019 by the American Academy of Orthopaedic Surgeons. "All Rights Reserved." This website and its contents may not be reproduced in whole or in part without written permission. "American Academy of Orthopaedic Surgeons" and its associated seal and "American Association of Orthopaedic Surgeons" and its logo are all registered U.S. trademarks and may not be used without written permission.
+          </small>
         </div>
       </footer>
     )
