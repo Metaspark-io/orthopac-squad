9d1fb27a7ce35b331d396654934339ccea8bfaf1
Sean Perkins <sean@mobility-labs.com>
2019-03-04 12:33:42 -0500

    Update every 3 seconds up to 10k more than starting

--- src/components/Ask.js
+++ src/components/Ask.js
@@ -2,9 +2,36 @@ import React, { Component } from 'react'
 
 import DONORS, { getTotalDonations } from '../constants/donors'
 
+const TOTAL_DONATIONS = getTotalDonations()
+
 export default class Ask extends Component {
+  state = {
+    totalDonations: TOTAL_DONATIONS,
+  }
+
+  // handle to cancel interval
+  intervalId = null
+
+  componentDidMount () {
+    this.intervalId = setInterval(
+      this.triggerDonationUpdate,
+      3000 // Its updated every 3 seconds
+    )
+  }
+
+  triggerDonationUpdate = () => {
+    const { totalDonations } = this.state
+    // Gives us a number between 1 and 5 times 1000
+    const amt = (Math.floor(Math.random() * 5) + 1) * 1000
+    this.setState({ totalDonations: totalDonations + amt })
+    // We only want to update up to 10k more than starting
+    if (totalDonations > TOTAL_DONATIONS + 10000) {
+      clearInterval(this.intervalId)
+    }
+  }
+
   render () {
-    const totalDonations = getTotalDonations()
+    const { totalDonations } = this.state
     return (
       <div className="ask-bg py-5">
         <div className="container">
