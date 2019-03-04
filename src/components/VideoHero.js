22adb11e0e34db30ef41d04ae57a826d95a48c25
Rick Metzger <rickymetz@gmail.com>
2019-03-04 16:17:22 -0500

    Change play button height, make issues component 90% wide

--- src/components/VideoHero.js
+++ src/components/VideoHero.js
@@ -8,20 +8,20 @@ import Play from '../images/videoHero/play.svg'
 export default class VideoHero extends Component {
   render () {
     return (
-      <div className="cover-image" style={{ height: 'calc(100vh - 110px)' }}>
+      <div className="cover-image">
         <div className="container d-flex flex-column align-items-center justify-content-center h-100">
           <div className="d-flex flex-column justify-content-center">
             <img className="align-self-center img-fluid p-4 mt-4 w-75" src={Title}/>
             <button className="btn align-self-center">
-              <img src={Play} alt="Play" style={{ height: 75 }}/>
+              <img className="play-icon" src={Play} alt="Play"/>
             </button>
           </div>
           <div className="row">
             <div class="col-6">
-              <img className="img-fluid" src={Left}/>
+              <img className="img-fluid" src={Left} alt="Mighty Microscope, Super Specialty, Skeletal Man"/>
             </div>
             <div class="col-6">
-              <img className="img-fluid" src={Right}/>
+              <img className="img-fluid" src={Right} alt="Ligament Lady, Ancillary Annie, Radical Reformer"/>
             </div>
           </div>
         </div>
