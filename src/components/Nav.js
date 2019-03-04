fafa2ff39209d3783daee596b7e36c590f1e8ff6
Rick Metzger <rickymetz@gmail.com>
2019-03-04 15:37:37 -0500

    Add secondary text to sticky nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,10 +6,22 @@ export default class Nav extends Component {
     return (
       <div className="container-fluid py-2 bg-light sticky-top">
         <nav className="nav nav-pills nav-fill">
-          <a className="nav-item nav-link" href="/#issues">The Issues</a>
-          <a className="nav-item nav-link" href="/#heroes">The Heroes</a>
-          <Link className="nav-item nav-link" activeClassName="active" to="/survey">Survey</Link>
-          <a className="nav-item nav-link" href="https://google.com" target="_blank">Join the Squad</a>
+          <a className="nav-item nav-link" href="/#issues">
+            <b>The Issues</b>
+            <span class="d-block"><small>Learn about the battles AAOS fights</small></span>
+          </a>
+          <a className="nav-item nav-link" href="/#heroes">
+            <b>The Heroes</b>
+            <span class="d-block"><small>Meet the OrthoPAC Squad!</small></span>
+          </a>
+          <Link className="nav-item nav-link" activeClassName="active" to="/survey">
+            <b>Survey</b>
+            <span class="d-block"><small>Got a minute? Discover your Ortho hero identity</small></span>
+          </Link>
+          <a className="nav-item nav-link" href="https://google.com" target="_blank">
+            <b>Join the Squad</b>
+            <span class="d-block"><small>Donate to be a Hero for medicine</small></span>
+          </a>
         </nav>
       </div>
     )
