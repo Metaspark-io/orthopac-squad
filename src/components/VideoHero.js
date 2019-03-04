09aa0eff357a912578ff011472f118698f1438dc
Rick Metzger <rickymetz@gmail.com>
2019-03-04 15:09:34 -0500

    Add play button to video cover

--- src/components/VideoHero.js
+++ src/components/VideoHero.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { FaPlay } from 'react-icons/fa'
 
 import Title from '../images/videoHero/title.png'
 import Left from '../images/videoHero/heroes-left.svg'
@@ -9,8 +10,11 @@ export default class VideoHero extends Component {
     return (
       <div className="cover-image" style={{ height: 'calc(100vh - 110px)' }}>
         <div className="container d-flex flex-column align-items-center justify-content-center">
-          <div className="d-flex justify-content-center">
+          <div className="d-flex flex-column justify-content-center">
             <img className="align-self-center img-fluid p-4 mt-4 w-75" src={Title}/>
+            <button className="btn btn-lg btn-primary align-self-center d-flex rounded-circle p-3">
+              <FaPlay className="pl-1"/>
+            </button>
           </div>
           <div className="row">
             <div class="col-6">
