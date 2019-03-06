c8f0c24eb28e0a520821b4fb6665fb1e62efa5c4
Rick Metzger <rickymetz@gmail.com>
2019-03-06 11:15:49 -0500

    Add Favicons

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -34,6 +34,16 @@ export default () => (
           content={data.site.siteMetadata.description}
         />
         <meta itemprop="image" content={data.site.siteMetadata.imageUrl} /> />
+        {/* Favicons */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png?v=xQdbY6n75P"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png?v=xQdbY6n75P"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png?v=xQdbY6n75P"/>
+        <link rel="manifest" href="/icons/site.webmanifest?v=xQdbY6n75P"/>
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg?v=xQdbY6n75P" color="#455bdc"/>
+        <link rel="shortcut icon" href="/icons/favicon.ico?v=xQdbY6n75P"/>
+        <meta name="msapplication-TileColor" content="#2d89ef"/>
+        <meta name="msapplication-config" content="/icons/browserconfig.xml?v=xQdbY6n75P"/>
+        <meta name="theme-color" content="#455bdc"/>
         {/* fonts */}
         <link href="https://fonts.googleapis.com/css?family=Arvo:400,700" rel="stylesheet"/>
       </Helmet>
