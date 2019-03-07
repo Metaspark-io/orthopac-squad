aff42fe9a61375c1304136eb4e51dd854471573e
Rick Metzger <rickymetz@gmail.com>
2019-03-07 07:51:03 -0700

    Add ll illustration behind thermometer, center ask text in divs

--- src/components/Ask.js
+++ src/components/Ask.js
@@ -4,6 +4,8 @@ import Thermometer from './Thermometer'
 
 import DONORS, { getTotalDonations } from '../constants/donors'
 
+import ll from '../images/ll-transparent.png'
+
 const TOTAL_DONATIONS = getTotalDonations()
 
 export class AskList extends Component {
@@ -42,11 +44,15 @@ export class AskList extends Component {
               <div className="card-body ask-card">
                 <div className="row">
                   <div className="col-12 col-md-6">
-                    <h2>The Squad needs you</h2>
-                    <p>Heroes wanted! Your OrthoPAC membership and donations help ensure our profession a powerful voice in government. Protect the autonomy of orthopeadists and the quality of the care we provide!</p>
-                    <div className="btn-toolbar">
-                      <a href="https://sso.aaos.org/login.aspx?vi=8&vt=77dc2d42411a9e98420dd4aaa5809106ee3572701588e5e6ee0c58f79c1d6ca84d304858ffc57b32d68b99ef404de6c6d5f0081e337c3594184003d8545161658382b3e80ad4a05a3907d076217b4ac42967966f94bda931557ebc73b9c211e4384993a31b7cdbd482b74314e270da05c29829fccad3fdd505f9d189bd304aad07cad1d45d7aab1734025ec7db4eb3ad5ac98358e895b25d09bc4b7ccdeca5f50993d7fe12cd216914078a761e6e8806ddd1cad026278ce5fc71b2d1eb5c4a11" className="btn btn-primary btn-lg mr-2 mb-2" rel="noopener noreferrer">Join the OrthoPac Squad</a>
-                      <a href="https://www.aaos.org/Advocacy/PAC/?ssopc=1" className="btn btn-secondary btn-lg mb-2" rel="noopener noreferrer">Donate to become a Hero</a>
+                    <div className="d-flex h-100 align-items-center">
+                      <div className="mb-4 mb-md-0">
+                        <h2>The Squad needs you</h2>
+                        <p>Heroes wanted! Your OrthoPAC membership and donations help ensure our profession a powerful voice in government. Protect the autonomy of orthopeadists and the quality of the care we provide!</p>
+                        <div className="btn-toolbar">
+                          <a href="https://sso.aaos.org/login.aspx?vi=8&vt=77dc2d42411a9e98420dd4aaa5809106ee3572701588e5e6ee0c58f79c1d6ca84d304858ffc57b32d68b99ef404de6c6d5f0081e337c3594184003d8545161658382b3e80ad4a05a3907d076217b4ac42967966f94bda931557ebc73b9c211e4384993a31b7cdbd482b74314e270da05c29829fccad3fdd505f9d189bd304aad07cad1d45d7aab1734025ec7db4eb3ad5ac98358e895b25d09bc4b7ccdeca5f50993d7fe12cd216914078a761e6e8806ddd1cad026278ce5fc71b2d1eb5c4a11" className="btn btn-primary btn-lg mr-2 mb-2" rel="noopener noreferrer">Join the OrthoPac Squad</a>
+                          <a href="https://www.aaos.org/Advocacy/PAC/?ssopc=1" className="btn btn-secondary btn-lg mb-2" rel="noopener noreferrer">Donate to become a Hero</a>
+                        </div>
+                      </div>
                     </div>
                   </div>
                   <div className="col-12 col-md-6">
@@ -126,33 +132,29 @@ export class AskThermometer extends Component {
             <div className="card card-comic">
               <div className="card-body ask-card">
                 <div className="row">
-                  <div className="col-12 col-md-6">
-                    <Thermometer
-                      max={2000000}
-                      size="large"
-                      height={400}
-                      value={totalDonations}
-                    />
-                  </div>
-                  <div className="col-12 col-md-6">
-                    <h2>The Squad needs you</h2>
-                    <p>Heroes wanted! Your OrthoPAC membership and donations help ensure our profession a powerful voice in government. Protect the autonomy of orthopeadists and the quality of the care we provide!</p>
-                    <div className="btn-toolbar">
-                      <a href="https://sso.aaos.org/login.aspx?vi=8&vt=77dc2d42411a9e98420dd4aaa5809106ee3572701588e5e6ee0c58f79c1d6ca84d304858ffc57b32d68b99ef404de6c6d5f0081e337c3594184003d8545161658382b3e80ad4a05a3907d076217b4ac42967966f94bda931557ebc73b9c211e4384993a31b7cdbd482b74314e270da05c29829fccad3fdd505f9d189bd304aad07cad1d45d7aab1734025ec7db4eb3ad5ac98358e895b25d09bc4b7ccdeca5f50993d7fe12cd216914078a761e6e8806ddd1cad026278ce5fc71b2d1eb5c4a11" className="btn btn-primary btn-lg mr-2 mb-2" rel="noopener noreferrer">Join the OrthoPac Squad</a>
-                      <a href="https://www.aaos.org/Advocacy/PAC/?ssopc=1" className="btn btn-secondary btn-lg mb-2" rel="noopener noreferrer">Donate to become a Hero</a>
+                  <div className="col-12 col-lg-6">
+                    <div className="p-relative p-4">
+                      <Thermometer
+                        max={2000000}
+                        size="large"
+                        height={400}
+                        value={totalDonations}
+                      />
+                      <img className="therm-illustration" src={ll} alt="Lady Ligament"/>
                     </div>
                   </div>
-                </div>
-                <div class="marquee">
-                  {
-                    // DONORS.oneK.map((donor, i) => (
-                    //   <span key={i} className="font-weight-bold">{donor}</span>
-                    // ))
-                  }{
-                    // DONORS.twoK.map((donor, i) => (
-                    //   <span key={i} className="font-weight-bold">{donor}</span>
-                    // ))
-                  }
+                  <div className="col-12 col-lg-6">
+                    <div className="d-flex h-100 align-items-center">
+                      <div className="pt-5 pt-md-0">
+                        <h2>The Squad needs you</h2>
+                        <p>Heroes wanted! Your OrthoPAC membership and donations help ensure our profession a powerful voice in government. Protect the autonomy of orthopeadists and the quality of the care we provide!</p>
+                        <div className="btn-toolbar">
+                          <a href="https://sso.aaos.org/login.aspx?vi=8&vt=77dc2d42411a9e98420dd4aaa5809106ee3572701588e5e6ee0c58f79c1d6ca84d304858ffc57b32d68b99ef404de6c6d5f0081e337c3594184003d8545161658382b3e80ad4a05a3907d076217b4ac42967966f94bda931557ebc73b9c211e4384993a31b7cdbd482b74314e270da05c29829fccad3fdd505f9d189bd304aad07cad1d45d7aab1734025ec7db4eb3ad5ac98358e895b25d09bc4b7ccdeca5f50993d7fe12cd216914078a761e6e8806ddd1cad026278ce5fc71b2d1eb5c4a11" className="btn btn-primary btn-lg mr-2 mb-2" rel="noopener noreferrer">Join the OrthoPac Squad</a>
+                          <a href="https://www.aaos.org/Advocacy/PAC/?ssopc=1" className="btn btn-secondary btn-lg mb-2" rel="noopener noreferrer">Donate to become a Hero</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
