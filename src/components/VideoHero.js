4a83243e1c4d12a0f59b80d3c6625d41cc699eb5
Rick Metzger <rickymetz@gmail.com>
2019-03-11 15:29:02 -0400

    Change tagline location

--- src/components/VideoHero.js
+++ src/components/VideoHero.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import VideoModal from './VideoModal'
 
 import Title from '../images/videoHero/title.png'
+import Tagline from '../images/videoHero/tagline.png'
 import Left from '../images/videoHero/heroes-left.svg'
 import Right from '../images/videoHero/heroes-right.svg'
 import Play from '../images/videoHero/play.svg'
@@ -26,7 +27,7 @@ export default class VideoHero extends Component {
         <div className="cover-image">
           <div className="container d-flex flex-column align-items-center justify-content-center h-100">
             <div className="d-flex flex-column justify-content-center">
-              <img className="align-self-center img-fluid p-4 mt-4 w-75" src={Title}/>
+              <img className="align-self-center img-fluid p-4 mt-4 w-75" src={Title} alt="Meet the OrthoPAC Squad"/>
               <button className="btn align-self-center play-button">
                 <img className="play-icon" src={Play} alt="Play" onClick={this.toggleOpen}/>
               </button>
@@ -40,6 +41,7 @@ export default class VideoHero extends Component {
               </div>
             </div>
           </div>
+          <img className="tagline " src={Tagline} alt="Advancing Orthopaedics for Good"/>
         </div>
         <VideoModal
           channel='vimeo'
