af716a95b5cac9264195aa60bf5b2cc0ed32c7d0
Rick Metzger <rickymetz@gmail.com>
2019-06-27 12:42:11 -0400

    Add static section link to comic inside of nav element

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,6 +5,8 @@ import { MdOpenInNew } from 'react-icons/md'
 import scrollToElement from 'scroll-to-element'
 import { OutboundLink } from 'gatsby-plugin-google-analytics'
 
+import Background from '../images/speedlines.png'
+
 export default class Nav extends Component {
   constructor (props) {
     super(props)
@@ -41,46 +43,66 @@ export default class Nav extends Component {
   render () {
     const navBreak = '960px'
     return (
-      <div className="container-fluid py-2 bg-white sticky-top border-bottom">
-        <nav className="nav nav-pills nav-fill">
-          <Link
-            className="nav-item nav-link"
-            onClick={e => this._handleLinkClick(e, '#issues')}
-            to={'/#issues'}
+      <React.Fragment>
+        <div className="container-fluid py-2 bg-white sticky-top border-bottom">
+          <nav className="nav nav-pills nav-fill">
+            <Link
+              className="nav-item nav-link"
+              onClick={e => this._handleLinkClick(e, '#issues')}
+              to={'/#issues'}
+            >
+              <b>The Issues</b>
+              <MediaQuery minWidth={navBreak}>
+                <span className="d-block"><small>Learn about the battles AAOS fights</small></span>
+              </MediaQuery>
+            </Link>
+            <Link
+              className="nav-item nav-link"
+              onClick={e => this._handleLinkClick(e, '#heroes')}
+              to={'/#heroes'}
+            >
+              <b>The Heroes</b>
+              <MediaQuery minWidth={navBreak}>
+                <span className="d-block"><small>Meet the OrthoPAC Squad!</small></span>
+              </MediaQuery>
+            </Link>
+            <Link
+              className="nav-item nav-link"
+              activeClassName="active"
+              to="/survey"
+            >
+              <b>Hero Quiz</b>
+              <MediaQuery minWidth={navBreak}>
+                <span className="d-block"><small>Got a minute? Discover your Ortho hero identity</small></span>
+              </MediaQuery>
+            </Link>
+            <OutboundLink className="nav-item nav-link" href="https://www.aaos.org/Advocacy/PAC/?ssopc=1" target="_blank" rel="noopener noreferrer">
+              <b>AAOS Orthopaedic PAC Home Page <MdOpenInNew/></b>
+              <MediaQuery minWidth={navBreak}>
+                <span className="d-block"><small>Learn more about OrthoPAC</small></span>
+              </MediaQuery>
+            </OutboundLink>
+          </nav>
+        </div>
+        <div
+          className="bg-primary text-center py-3 border-bottom"
+          style={{
+            background: `url(${ Background })`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          <a
+            className="text-white"
+            href="/orthopac-comic.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <b>The Issues</b>
-            <MediaQuery minWidth={navBreak}>
-              <span className="d-block"><small>Learn about the battles AAOS fights</small></span>
-            </MediaQuery>
-          </Link>
-          <Link
-            className="nav-item nav-link"
-            onClick={e => this._handleLinkClick(e, '#heroes')}
-            to={'/#heroes'}
-          >
-            <b>The Heroes</b>
-            <MediaQuery minWidth={navBreak}>
-              <span className="d-block"><small>Meet the OrthoPAC Squad!</small></span>
-            </MediaQuery>
-          </Link>
-          <Link
-            className="nav-item nav-link"
-            activeClassName="active"
-            to="/survey"
-          >
-            <b>Hero Quiz</b>
-            <MediaQuery minWidth={navBreak}>
-              <span className="d-block"><small>Got a minute? Discover your Ortho hero identity</small></span>
-            </MediaQuery>
-          </Link>
-          <OutboundLink className="nav-item nav-link" href="https://www.aaos.org/Advocacy/PAC/?ssopc=1" target="_blank" rel="noopener noreferrer">
-            <b>AAOS Orthopaedic PAC Home Page <MdOpenInNew/></b>
-            <MediaQuery minWidth={navBreak}>
-              <span className="d-block"><small>Learn more about OrthoPAC</small></span>
-            </MediaQuery>
-          </OutboundLink>
-        </nav>
-      </div>
+            Read about Skeletal Man's Origin Story, in this exclusive comic book! <MdOpenInNew/>
+          </a>
+        </div>
+      </React.Fragment>
     )
   }
 }
